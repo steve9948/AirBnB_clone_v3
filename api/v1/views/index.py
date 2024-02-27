c8d5abcede821a6fce7cc2c399841144310d252b
steve9948 <mbuguagichimu@gmail.com>
2024-02-27 02:11:02 -0800

    Doc issue

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-'''
-Handles the starting of an api
-'''
+"""define routes of blueprint
+"""
 
 from api.v1.views import app_views
 from models import storage
@@ -15,7 +14,9 @@ from models.user import User
 
 @app_views.route("/status", strict_slashes=False, methods=["GET"])
 def status():
-    return "OK"
+    return {
+        "status": "OK",
+    }
 
 
 @app_views.route("/stats", strict_slashes=False, methods=["GET"])
@@ -26,7 +27,6 @@ def stats():
     reviews = storage.count(Review)
     states = storage.count(State)
     users = storage.count(User)
-    """Retrieves the number of each object by type."""
     return {
         "amenities": amenities,
         "cities": cities,
